8a47cf87771616d1abd090d9c1bc0338611936db
Mitchenya <Mitchellrjshepherd@outlook.com>
2024-08-30 12:56:29 +0100

    This commit will complete 04-get-api-articles-article-id, and alter a few pieces of code from task 3

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller");
-const { serverErrorHandler } = require("./error");
 const { getEndPoints } = require("./controllers/api.controller");
+const { getArticleById } = require("./controllers/articles.controllers");
+const {
+  serverErrorHandler,
+  customErrorHandler,
+  idErrorHandler,
+} = require("./error");
 
 app.get("/api/topics", getTopics);
 app.get("/api", getEndPoints);
+app.get("/api/articles/:article_id", getArticleById);
+app.use(customErrorHandler);
+app.use(idErrorHandler);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "Route not found" });
